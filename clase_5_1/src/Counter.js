fc0da149b8b9f5840f998817d2e68ae77bf80650
Juan Baranovsky <xxbaraxx@hotmail.com>
2022-08-07 00:34:28 -0300

    final clase 5

---
+++ clase_5_1/src/Counter.js
@@ -0,0 +1,76 @@
+import React, { useReducer } from 'react';
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './style.css';
+
+//? primer mal reducer
+// const reducer = (state, value) => {
+//     return state + value;
+// }
+
+const reducer = (state, action) => {
+    switch (action) {
+        case 'sumar':
+            return {
+                ...state,
+                count: state.count + 1
+            };
+        case 'restar':
+            return {
+                ...state,
+                count: state.count - 1 < 1 ? 0 : state.count - 1,
+                error: state.count < 1 ? 'Error: el contador debe ser mayor a 0' : null,
+            }
+        case 'borrarError':
+            return {
+                ...state,
+                error: null,
+            }
+        default:
+            return state;
+    }
+}
+
+
+const Counter = () => {
+    const [{ count, error }, dispatch] = useReducer(reducer, { count: 0, error: null });
+
+
+    const handlerSubstrack = () => {
+        dispatch('restar');
+    }
+
+    const handlerAdd = () => {
+        dispatch('sumar');
+    }
+
+    const elemento = (<div>
+        <div className='counter'>
+            <div>
+                <button type='button' className='btn' onClick={handlerSubstrack}>
+                    <FontAwesomeIcon icon={faMinus} />
+                </button>
+            </div>
+            <div>{count}</div>
+            <div>
+                <button type='button' className='btn' onClick={handlerAdd}>
+                    <FontAwesomeIcon icon={faPlus} />
+                </button>
+            </div>
+        </div>
+        {error && (
+            <div style={{ margin: '20px' }}>
+                <center>
+                    <button className='btn' onClick={() => dispatch('borrarError')}>Quitar Error</button>
+                    <p style={{ color: 'red' }}>{error}</p>
+                </center>
+            </div>
+        )}
+    </div>
+    );
+    console.log(elemento)
+
+    return elemento;
+};
+
+export default Counter;
