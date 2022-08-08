dd4721fc50307a9a9c9efd192d761493a68ecaf5
Juan Baranovsky <xxbaraxx@hotmail.com>
2022-08-08 20:07:32 -0300

    clase7 no me funciono del todo

---
+++ clase_7/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styles from './App.module.css'
+import Header from './components/header/Header'
+import Home from './pages/home/Home'
+import Posts from './pages/posts/Posts'
+
+export default function App() {
+    return (
+        <Router>
+            <div className={styles.App}>
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/posts">
+                        <Posts />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
